2f2c632fddf0c28eb9c903e2c362368082f239a6
mballesterosv <16maarioo16@gmail.com>
2024-02-20 20:54:58 +0100

    clase, falta la refencia a css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {RentalsComponent} from "./home/rentals/rentals.component";
-import {SinglepropertyComponent} from "./home/rentals/singleproperty/singleproperty.component";
 import {AddrentalComponent} from "./home/addrental/addrental.component";
+import {SinglepropertyComponent} from "./home/rentals/singleproperty/singleproperty.component";
 import {IndexComponent} from "./home/index/index.component";
 import {SigninComponent} from "./auth/signin/signin.component";
 import {SignupComponent} from "./auth/signup/signup.component";
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
  // { path: 'allproperties', component: AllPropertiesComponent }
-  { path: 'home/rentals', component: RentalsComponent },
+  { path: 'games', component: RentalsComponent },
   { path: 'home/addproperty', component: AddrentalComponent },
   // { path: 'home/enquiries', component: EnquiriesComponent },
  // { path: 'home/myproperties', component: MyPropertiesComponent },
@@ -20,8 +20,6 @@ const routes: Routes = [
   { path: 'index', component: IndexComponent },
   { path: 'index/login', component: SigninComponent },
   { path: 'index/register', component: SignupComponent },
-
-
 
 ];
 
